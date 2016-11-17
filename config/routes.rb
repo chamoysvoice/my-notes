fec5f0eba224ec95b25de91a9a8c553affd93e08
Jesus Garlea <chamoysvoice@gmail.com>
2016-11-16 22:57:19 -0600

    Redirect to notes when authenticated

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   resources :notes
   get 'landing/index'
 
+  authenticated :user do
+    root 'notes#index', as: 'authenticated_root'
+  end
+  
   root 'landing#index'
 end
