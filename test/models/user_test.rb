a40dfe62f9e8cf52cd495346609a905c9fb8b248
Jesus Garlea <chamoysvoice@gmail.com>
2016-11-21 01:02:18 -0600

    Test: user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'empty user' do
+    user = User.new
+    refute user.save
+  end
 end
