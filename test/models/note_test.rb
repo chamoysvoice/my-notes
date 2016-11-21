10efb8bc66ed3909199048b2f3a9bb7a1facd9a2
Jesus Garlea <chamoysvoice@gmail.com>
2016-11-21 01:02:36 -0600

    Test: note model

--- test/models/note_test.rb
+++ test/models/note_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class NoteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:one)
+  end
+
+  test 'empty note' do
+    note = Note.new
+    refute note.save
+  end
+
+  test 'complete note' do
+    note = Note.new(title: 'foo', content: 'bar', user: @user)
+    assert note.save
+  end
+
+  test 'user empty note' do
+    note = Note.new(user: @user)
+    refute note.save
+  end
 end
